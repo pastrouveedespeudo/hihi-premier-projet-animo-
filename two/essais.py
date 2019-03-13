941c118e08ec7654849b189ee87a6d4a7bd628de
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-13 19:50:25 +0100

    Update essais.py

--- two/essais.py
+++ two/essais.py
@@ -64,32 +64,55 @@ class yo :
                
         im = cv2.imread(self.image)
         c = 0
-        for i in a[1][24]:
+        for i in a[1]:
             if i == []:
                 pass
             else:
-                print(i)
-                print(len(a[1][5]))
-                im[i[0], i[1]] = 0,0,255
+                if len(a[1][c]) > 10:
+                    for i in a[1][c]:
+                        im[i[0], i[1]] = 0,0,255
+                        
+                    print("rangée :", c)
+                    
+                
                 c+=1
+
+        #cv2.imshow("image.jpg", im)
+        cv2.imwrite("recup.jpg", im)
+
+
         
-        cv2.imshow("image.jpg", im)
-        #if y est a coté on compte pas !
+        im_recup = cv2.imread("recup.jpg")
+        for x in range(im.shape[0]):
+            for y in range(im.shape[1]):
+                if im[x,y][0] == 255 and im[x,y][1] != 255 and im[x,y][2] != 255:
+                    im[x,y] = 0,0,0
+
+        
+        cv2.imshow("image.jpg", im_recup)
+        cv2.imwrite("recup2.png", im)
+        shutil.move("recup2.png", r"C:\Users\jeanbaptiste\coatis")
 
 
 
+    def obtenir_pts_blanc(self):
 
-
-
-
-
-
-
+        os.chdir(r"C:\Users\jeanbaptiste\coatis")
+        im = cv2.imread("recup2.png")
+        for x in range(im.shape[0]):
+            for y in range(im.shape[1]):
+                try:
+                    if im[x+1,y].all() == np.array([0,0,255]).all() and\
+                        im[x+1,y].all() == np.array([0,0,255]).all():
+                        pass
+                    else:
+                        im[x,y] = 0,0,0
+                except:
+                    pass
+        cv2.imwrite("recup2.jpg", im)
 
 if __name__ == "__main__":
     
-
-
 
     yo = yo()
 
@@ -100,10 +123,11 @@ if __name__ == "__main__":
 
     pos_im=yo.recup_pixel(liste_dossier[0])
     yo.grigri(pos_im,liste_dossier[0])
+    yo.obtenir_pts_blanc()
 
 
-
-
+#bon a la base je voulais ca mais je comprend pas comment ca se fait qu'il est 4 petit pts blanc au milieu voir si ca marche
+#avec toutes les images... et pk y'a des pts blanc qu ireste
 
 
 
